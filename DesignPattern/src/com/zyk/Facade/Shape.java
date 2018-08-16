003b0cb293aab1a83c45f9eda5ecdc62a50da922
zyk-um <1251134350@qq.com>
2018-08-16 17:44:15 +0800

    增加外观模式

---
+++ DesignPattern/src/com/zyk/Facade/Shape.java
@@ -0,0 +1,5 @@
+package com.zyk.Facade;
+
+public interface Shape {
+    void draw();
+}
---
+++ DesignPattern/src/com/zyk/Facade/Square.java
@@ -0,0 +1,8 @@
+package com.zyk.Facade;
+
+public class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Square::draw()");
+    }
+}
