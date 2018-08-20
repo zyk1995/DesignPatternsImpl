74841643efad75cd1973e9e94b7a70ddfcceb96e
zyk-um <1251134350@qq.com>
2018-08-20 18:03:09 +0800

    增加责任链模式

---
+++ DesignPattern/src/com/zyk/ChainOfResponsibility/AbstractLogger.java
@@ -0,0 +1,31 @@
+package com.zyk.ChainOfResponsibility;
+
+
+/*
+ *  责任链模式（Chain of Responsibility Pattern）为请求创建了一个接收者对象的链。这种模式给予请求的类型，对请求的发送者和接收者进行解耦。这种类型的设计模式属于行为型模式。
+ *  在这种模式中，通常每个接收者都包含对另一个接收者的引用。如果一个对象不能处理该请求，那么它会把相同的请求传给下一个接收者，依此类推。
+ */
+public abstract class AbstractLogger {
+    public static int INFO = 1;
+    public static int DEBUG = 2;
+    public static int ERROR = 3;
+
+    protected int level;
+
+    protected AbstractLogger nextLogger;
+
+    public void setNextLogger(AbstractLogger nextLogger) {
+        this.nextLogger = nextLogger;
+    }
+
+    public void logMessage(int level, String message) {
+        if(this.level <= level) {
+            write(message);
+        }
+        if(nextLogger != null) {
+            nextLogger.logMessage(level, message);
+        }
+    }
+
+    abstract protected void write(String message);
+}
