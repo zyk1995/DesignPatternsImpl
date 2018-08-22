633f6dd0a99a53846c9d713af544d6f16e631dc4
zyk-um <1251134350@qq.com>
2018-08-22 11:25:35 +0800

    增加前端控制器模式

---
+++ DesignPattern/src/com/zyk/frontController/Main.java
@@ -0,0 +1,9 @@
+package com.zyk.frontController;
+
+public class Main {
+    public static void main(String[] args) {
+        FrontController frontController = new FrontController();
+        frontController.dispatchRequest("HOME");
+        frontController.dispatchRequest("STUDENT");
+    }
+}
---
+++ DesignPattern/src/com/zyk/frontController/StudentView.java
@@ -0,0 +1,7 @@
+package com.zyk.frontController;
+
+public class StudentView {
+    public void show(){
+        System.out.println("Displaying Student Page");
+    }
+}
